9eceddcc777f22c7556e03ad3b5c98c505c2c98f
Bisri Hanafi <bisrihanafi@gmail.com>
2018-12-08 15:25:54 +0700

    Penambahan fragment pada beranda sehingga mempilkan 3 tab (personal, Akademik, dan Organisasi).

--- app/src/main/java/com/example/com/caminfo/Beranda.java
+++ app/src/main/java/com/example/com/caminfo/Beranda.java
@@ -2,9 +2,9 @@ package com.example.com.caminfo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,7 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
@@ -33,9 +32,22 @@ public class Beranda extends AppCompatActivity implements NavigationView.OnNavig
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
-
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(PersonalFragment.newInstance("Personal"), "Personal");
+        adapter.addFragment(AkademikFragmen.newInstance("Akademik"), "Akademik");
+        adapter.addFragment(AkademikFragmen.newInstance("Organisasi"), "Organisasi");
+        viewPager.setAdapter(adapter);
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
